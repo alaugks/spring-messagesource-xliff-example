518e51aab5310e1934f09b12ae0bbb341859522b
André Laugks <al@pb82.de>
2024-08-08 11:25:24 +0200

    Disable rule java:S1192

--- src/main/java/io/github/alaugks/controller/HomeController.java
+++ src/main/java/io/github/alaugks/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@SuppressWarnings({"java:S1192"})
 @Controller
 @RequestMapping("/{lang}")
 public class HomeController {
