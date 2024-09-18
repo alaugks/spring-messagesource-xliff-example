2eea9f536ecfa12177a3ba096e336286fc64e2a1
André Laugks <al@pb82.de>
2024-09-18 19:51:54 +0200

    Add version spring-messagesource-xliff:2.0.0

--- src/main/java/io/github/alaugks/controller/IndexController.java
+++ src/main/java/io/github/alaugks/controller/IndexController.java
@@ -1,6 +1,7 @@
 package io.github.alaugks.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,6 +11,7 @@ public class IndexController {
 
     @GetMapping("/")
     public ModelAndView index(HttpServletRequest request) {
+        // Fallback. Redirect is by RequestURILocaleInterceptor.
         String langCode = request.getLocale().toString();
         return new ModelAndView("redirect:" + String.format("/%s/home", langCode));
     }
