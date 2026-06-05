7b2194696fae6088faac32eac2dc5ae1e74813a5
André Laugks <alaugks@gmail.com>
2026-06-05 17:23:01 +0200

    Update translations and profiles
    - Add dev and test profiles to application properties
    - Update xliff versions and structure for translation files
    - Change project version to 3.0.0-SNAPSHOT in pom.xml
    - Refactor MessageSourceConfig to support environment-specific beans

--- src/main/java/io/github/alaugks/config/MessageSourceConfig.java
+++ src/main/java/io/github/alaugks/config/MessageSourceConfig.java
@@ -1,6 +1,7 @@
 package io.github.alaugks.config;
 
 import io.github.alaugks.spring.messagesource.catalog.resources.LocationPattern;
+import io.github.alaugks.spring.messagesource.xliff.XliffResourceMessageSource.Builder;
 import java.util.Locale;
 
 import io.github.alaugks.spring.messagesource.xliff.XliffResourceMessageSource;
@@ -8,17 +9,31 @@ import io.github.alaugks.spring.messagesource.xliff.XliffResourceMessageSource;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MessageSourceConfig {
 
-	@Bean
-	public MessageSource messageSource() {
+	public Builder createXliffResourceMessageSourceBuilder() {
 		return XliffResourceMessageSource
-				.builder(
-					Locale.forLanguageTag("en"),
-					new LocationPattern("translations/*")
-				)
-				.build();
+			.builder(
+				Locale.forLanguageTag("en"),
+				new LocationPattern("translations/*")
+			);
+	}
+
+	@Bean(name = "messageSource")
+	@Profile("dev")
+	public MessageSource messageSourceDev() {
+		return this.createXliffResourceMessageSourceBuilder()
+			.validateSchema(true)
+			.build();
+	}
+
+	@Bean
+	@Profile("!dev")
+	public MessageSource messageSource() {
+		return this.createXliffResourceMessageSourceBuilder()
+			.build();
 	}
 }
